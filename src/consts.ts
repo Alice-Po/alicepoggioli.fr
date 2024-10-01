325c0c0fdc88ee24f578c40a69d970173646e355
Alice Poggioli <alice.poggioli@assemblee-virtuelle.org>
2024-10-01 17:23:18 +0200

    update web site constants

--- src/consts.ts
+++ src/consts.ts
@@ -2,21 +2,21 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "Alice Poggioli",
+  DESCRIPTION: "Entrepreneuse web indépendante",
+  AUTHOR: "Alice Poggioli",
 }
 
 // Work Page
 export const WORK: Page = {
-  TITLE: "Work",
-  DESCRIPTION: "Places I have worked.",
+  TITLE: "Éxpériences professionnelles",
+  DESCRIPTION: "",
 }
 
 // Blog Page
 export const BLOG: Page = {
   TITLE: "Blog",
-  DESCRIPTION: "Writing on topics I am passionate about.",
+  DESCRIPTION: "",
 }
 
 // // Projects Page 
@@ -27,8 +27,8 @@ export const BLOG: Page = {
 
 // Search Page
 export const SEARCH: Page = {
-  TITLE: "Search",
-  DESCRIPTION: "Search all posts and projects by keyword.",
+  TITLE: "Rechercher",
+  DESCRIPTION: "",
 }
 
 // Links
@@ -53,29 +53,23 @@ export const LINKS: Links = [
 
 // Socials
 export const SOCIALS: Socials = [
-  { 
-    NAME: "Email",
-    ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
-  },
+  // { 
+  //   NAME: "Email",
+  //   ICON: "email", 
+  //   TEXT: "markhorn.dev@gmail.com",
+  //   HREF: "mailto:markhorn.dev@gmail.com",
+  // },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "Alice-Po",
+    HREF: "https://github.com/Alice-Po"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
-  },
-  { 
-    NAME: "Twitter",
-    ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
+    TEXT: "alice-poggioli",
+    HREF: "https://www.linkedin.com/in/alice-poggioli/",
+  }
 ]
 
