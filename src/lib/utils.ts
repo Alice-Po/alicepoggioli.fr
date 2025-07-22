79285d76bab640ae20f4b2bd88e4932b2622006b
Alice <alpog@ik.me>
2025-07-22 14:25:34 +0200

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,6 @@ export function cn(...inputs: ClassValue[]) {
 
 export function formatDate(date: Date) {
   return Intl.DateTimeFormat("fr-FR", {
-    day: "2-digit",
     month: "long",
     year: "numeric"
   }).format(date)
